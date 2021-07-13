51ba546a9c5374fea97141a8f1d89920a53b5a60
azeezabidoye <azeezabidoye@yahoo.com>
2021-07-13 13:45:57 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,24 @@
+const express = require("express");
+let cors = require("cors");
+const data = require("./load-data")
+
+const app = express();
+const port = process.env.PORT || 5050;
+
+app.use(cors());
+
+
+app.get("/", (req, res) => {
+  res.send("Welcome to the Awesome Users API!");
+});
+
+app.get("/users/get", async (req, res) => {
+  const users = data.Users;
+
+  res.status(200).send(users);
+});
+
+
+app.listen(port, () => {
+  console.log(`App listening at http://localhost:${port}`);
+});
